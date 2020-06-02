bec83acde26cd6773f2b384982034f9af7117efd
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-06-02 21:37:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,14 +1,14 @@
 def count_strings(array)
-  array.count do |element|
-    element.class == String
+  array.count do |array|
+    array.class == String
   end
 end
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
-  array.count do |element|
-    element == ""
+  array.count do |array|
+    array == ""
   end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
